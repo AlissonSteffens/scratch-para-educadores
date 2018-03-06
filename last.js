93a77beb49b1f6faaf4956eafd92b5d167e856ec
AlissonSteffens <alisson.steffens@hotmail.com>
2018-03-06 15:31:03 -0300

    oficinas

---
+++ last.js
@@ -0,0 +1,20 @@
+var oficinas = [
+  {
+    nome: 'Campus Party',
+    local: 'SP',
+    data: '31 jan - 03 fev',
+    link: 'http://brasil.campus-party.org/'
+  },
+  {
+    nome: 'BC Smart Week',
+    local: 'SC',
+    data: '05 mar e 07 mar',
+    link: 'https://www.facebook.com/bccriativo/'
+  },
+  {
+    nome: 'Festival de Invenção e Criatividade',
+    local: 'SP',
+    data: '13 a 15 mar',
+    link: 'http://www.ficmaker.org.br/2018/'
+  }
+]
